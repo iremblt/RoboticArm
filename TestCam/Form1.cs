86a81aaf9edf2993e219fbdb187fc5a472b528ab
iremblt <irem_bulut_626@hotmail.com>
2022-01-19 21:09:21 +0300

    Add project files.

---
+++ TestCam/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+using Emgu.CV.Structure;
+using Emgu.CV; // It provides works capture,images functions.
+
+namespace TestCam
+{
+    public partial class Form1 : Form
+    {
+        private Capture capture; //Points to the camera
+        private Image<Bgr, Byte> IMG; //List of blue,green and red images. This is orginal image
+        private Image<Gray, Byte> R_frame;
+        private Image<Gray, Byte> G_frame;
+        private Image<Gray, Byte> B_frame;
+        private Image<Gray, Byte> GrayImg;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void processFrame(object sender, EventArgs e)
+        {
+            if (capture == null) //There is no object of a camera. You are not connect to the camera.
+            {
+                try //Try to create to the object or try to connect to the camera.
+                {
+                    capture = new Capture(); 
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            IMG = capture.QueryFrame(); //get a picture from a camera
+            R_frame = IMG[2].Copy();
+            G_frame = IMG[1].Copy();
+            B_frame = IMG[0].Copy();            
+            GrayImg = IMG.Convert<Gray, Byte>();
+  
+            try
+            {
+                imageBox1.Image = IMG;
+                imageBox2.Image = GrayImg;
+                imageBox3.Image = R_frame;
+                imageBox4.Image = G_frame;
+                imageBox5.Image = B_frame;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Idle += processFrame; //It provides walking in the background the images
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Idle -= processFrame; //It stops
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }    
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IMG.Save("Image" +  ".jpg");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
